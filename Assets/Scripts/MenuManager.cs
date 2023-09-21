93387354bffba461c65fab94fbd4d3996e9f5383
fer5899 <fdometallic@gmail.com>
2023-09-21 12:21:38 +0200

    Create start menu

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public GameObject menu;
+    public GameObject credits;
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void ShowCredits()
+    {
+        menu.SetActive(false);
+        credits.SetActive(true);
+    }
+
+    public void ShowMenu()
+    {
+        menu.SetActive(true);
+        credits.SetActive(false);
+    }
+}
