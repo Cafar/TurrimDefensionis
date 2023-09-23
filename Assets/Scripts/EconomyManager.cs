c20bca21c8b19367531883846842c4b665c2bd71
fer5899 <fdometallic@gmail.com>
2023-09-23 12:10:23 +0200

    Create economy manager

---
+++ Assets/Scripts/EconomyManager.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EconomyManager : MonoBehaviour
+{
+    [Header("Configuration")]
+    public int initialCoin = 0;
+    public int maxCoin = 1000;
+
+    [Space(10)]
+
+    [Header("Testing")]
+    public int currentCoin;
+
+    private GameManager gm;
+
+    private void OnEnable()
+    {
+        GameManager.onEndNight += GameManager_OnEndNight;
+    }
+
+    private void GameManager_OnEndNight()
+    {
+        
+    }
+
+    void Start()
+    {
+        currentCoin = initialCoin;
+        gm = GameObject.Find("GameManager").GetComponent<GameManager>();
+
+    }
+
+    // public function to buy tower
+    public bool SpendCoin(int coinToSpend)
+    {
+        if (coinToSpend > currentCoin)
+            return false;
+        currentCoin -= coinToSpend;
+        return true;
+    }
+
+    // function to get money at end of night
+    private void GainCoin(int coinToGain)
+    {
+        if (currentCoin + coinToGain > maxCoin)
+            currentCoin = maxCoin;
+        else
+            currentCoin += coinToGain;
+    }
+
+}
